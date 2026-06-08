874b642845c884f7e9353f9363c1163c70b45c38
Klawns <fabriciioklaus@gmail.com>
2026-06-08 12:10:52 -0300

    Fix PWA manifest icon assets

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -9,6 +9,16 @@ const APP_SHELL = [
   '/pwa-192x192.png',
   '/pwa-512x512.png',
   '/pwa-maskable-512x512.png',
+  '/icons/icon-48x48.png',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-256x256.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png',
 ]
 
 self.addEventListener('install', (event) => {
